cf3fdcc2d63989248831d7e3eac593fad0b392bf
Daniel DeGuire <danjdeguire@gmail.com>
2022-07-20 22:05:44 -0500

    first commit

--- 01-birthday-reminder/setup/src/data.js
+++ 01-birthday-reminder/setup/src/data.js
@@ -1,37 +1,37 @@
 export default [
   {
     id: 1,
-    name: 'Bertie Yates',
+    name: "David Cope",
     age: 29,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
   },
   {
     id: 2,
-    name: 'Hester Hogan',
+    name: "Jeff Kruz",
     age: 32,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
   },
   {
     id: 3,
-    name: 'Larry Little',
+    name: "Sara Kruz",
     age: 36,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
   },
   {
     id: 4,
-    name: 'Sean Walsh',
+    name: "Billy DeGuire",
     age: 34,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
   },
   {
     id: 5,
-    name: 'Lola Gardner',
+    name: "Jane DeGuire",
     age: 29,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
   },
 ];
